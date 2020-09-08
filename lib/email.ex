596dde1a2593861dec31e20eceeb793707b3c16d
jjl <james.laver@gmail.com>
2020-09-08 05:17:10 +0000

    v0.1.0

--- lib/email.ex
+++ lib/email.ex
@@ -11,17 +11,33 @@ defmodule CommonsPub.Emails.Email do
   
   mixin_schema do
     field :email, :string
+    field :email_confirm_token, :string
+    field :email_confirmed_at, :utc_datetime_usec
   end
 
   @defaults [
     cast:     [:email],
     required: [:email],
-    email: [ format: ~r(^[^@]{1,128}@[^@\.]+.[^@]{2,128}$) ],
+    email: [ format: ~r(^[^@]{1,128}@[^@\.]+\.[^@]{2,128}$) ],
   ]
 
   def changeset(email \\ %Email{}, attrs, opts \\ []) do
     Changesets.auto(email, attrs, opts, @defaults)
+    |> put_token()
     |> Changeset.unique_constraint(:email)
+  end
+
+  def put_token(changeset)
+  def put_token(%Changeset{valid?: true, changes: %{email: email}}=changeset) do
+    token = Base.encode64(:crypto.strong_rand_bytes(16), padding: false)
+    Changeset.put_change(changeset, :email_confirm_token, token)
+  end
+  def put_token(%Changeset{}=changeset), do: changeset
+
+  def confirm(%Email{}=email) do
+    email
+    |> Changeset.cast(%{},[])
+    |> Changeset.change(email_confirm_token: nil, email_confirmed_at: DateTime.utc_now())
   end
 
 end
@@ -37,13 +53,17 @@ defmodule CommonsPub.Emails.Email.Migration do
 
   def migrate_email(index_opts, :up) do
     create_mixin_table(Email) do
-      add :email, :text
+      add :email, :text, null: false
+      add :email_confirm_token, :text
+      add :email_confirmed_at, :timestamptz
     end
     create_if_not_exists(unique_index(@email_table, [:email], index_opts))
+    create_if_not_exists(unique_index(@email_table, [:email_confirm_token], index_opts))
   end
 
   def migrate_email(index_opts, :down) do
-    drop_if_exists(unique_index(@email_table, [:username], index_opts))
+    drop_if_exists(unique_index(@email_table, [:email_confirm_token], index_opts))
+    drop_if_exists(unique_index(@email_table, [:email], index_opts))
     drop_mixin_table(Email)
   end
 
--- mix.exs
+++ mix.exs
@@ -7,6 +7,20 @@ defmodule CommonsPub.Emails.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      description: "Email-related models for commonspub",
+      homepage_url: "https://github.com/commonspub/cpub_emails",
+      source_url: "https://github.com/commonspub/cpub_emails",
+      package: [
+        licenses: ["MPL 2.0"],
+        links: %{
+          "Repository" => "https://github.com/commonspub/cpub_emails",
+          "Hexdocs" => "https://hexdocs.pm/cpub_emails",
+        },
+      ],
+      docs: [
+        main: "readme", # The first page to display from the docs 
+        extras: ["README.md"], # extra pages to include
+      ],
       deps: deps()
     ]
   end
@@ -21,10 +35,10 @@ defmodule CommonsPub.Emails.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:pointers, "~> 0.4.2"},
-      {:pointers, git: "https://github.com/commonspub/pointers", branch: "main"},
-      # {:pointers, path: "../pointers"},
-      # {:flexto, "~> 0.2"},
+      {:pointers, "~> 0.5.1"},
+      # {:pointers, git: "https://github.com/commonspub/pointers", branch: "main"},
+      # {:pointers, path: "../pointers", override: true},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 end
